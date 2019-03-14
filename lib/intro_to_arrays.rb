692f2c075f57d076abda4282df73f2ce4f816c39
Michelle M <michellemoon236@gmail.com>
2019-03-14 20:47:10 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,28 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements (object1, object2)
+  Array.new(object1, object2)
+end
+
+def first_element (array)
+  array.first
+end
+
+def third_element (array)
+  array[2]
+end
+
+def last_element (array)
+  array[-1]
+end
+
+def first_element_with_array_methods (array)
+  array.first 
+end
+
+def last_element_with_array_methods (array)
+  array.last_element_with_array_methods
+end
+
